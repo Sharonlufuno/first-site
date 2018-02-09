470b5109bfc6d855afdc3baa94fb79ccb43b80fb
Sharonlufuno <36259235+Sharonlufuno@users.noreply.github.com>
2018-02-09 11:25:18 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,29 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/nash.jpg') {
+      myImage.setAttribute ('src','images/nash2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/nash.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome To My Website!, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome Back!, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
